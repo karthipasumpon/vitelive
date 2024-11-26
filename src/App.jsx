09d2c2779853d2847a73d6ac16bccdfdbe1be6db
akashnewport <shaliniragu53@gmail.com>
2024-11-26 12:43:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Main Site</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt porro aut omnis delectus maxime velit excepturi, voluptatem reprehenderit laborum, soluta quidem quo! Nulla vitae dignissimos excepturi doloremque atque. Reprehenderit, ut?</p>
+    </div>
+  )
+}
+
+export default App
